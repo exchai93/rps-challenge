acfe87f05590d4142ea073d431ab47dfc45c22ee
Emily Chai <echai93@gmail.com>
2017-04-02 09:54:57 +0100

    /names /player_name /play routes

--- app/rps_web.rb
+++ app/rps_web.rb
@@ -12,5 +12,23 @@ class Rps < Sinatra::Base
   get '/start_game' do
     erb(:start)
   end
-  
+
+  post '/names' do
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    redirect to('/player_name')
+  end
+
+  get '/player_name' do
+    erb(:player_name)
+  end
+
+  get '/play' do
+    erb(:play)
+  end
+
+  post '/choice' do
+
+  end
+
 end
